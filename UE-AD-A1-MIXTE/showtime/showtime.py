f278781dc3c83e1d240a3f16dda3d5cb3d1f9e6d
Sarah <105355912+SarahBlevin@users.noreply.github.com>
2024-10-20 01:01:40 +0200

    Update showtime.py

--- UE-AD-A1-MIXTE/showtime/showtime.py
+++ UE-AD-A1-MIXTE/showtime/showtime.py
@@ -4,12 +4,15 @@ import showtime_pb2
 import showtime_pb2_grpc
 import json
 
+# Showtime is a gRPC server that will be called by a client in service Bookings
+
 class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
 
     def __init__(self):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
-
+            
+    # get all movies on air at a specific date
     def GetShowtimeByDate(self, request, context):
         for date in self.db:
             if date['date'] == str(request.date):
@@ -17,10 +20,12 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
                 return showtime_pb2.Schedule(date=date['date'], movies=date['movies'])
         return showtime_pb2.Schedule(date="", movies=[])
 
+    # get all showtimes
     def GetShowtimes(self, request, context):
         for date in self.db:
             yield showtime_pb2.Schedule(date=date['date'], movies=date['movies'])
 
+# gRPC server definition and start
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
