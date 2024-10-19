02276773dc57d3bc1f876f3c2bfe75aeba0b47a4
Sarah <105355912+SarahBlevin@users.noreply.github.com>
2024-10-20 01:16:06 +0200

    Update bookingServer.py

--- UE-AD-A1-MIXTE/booking/bookingServer.py
+++ UE-AD-A1-MIXTE/booking/bookingServer.py
@@ -5,6 +5,9 @@ import booking_pb2
 import booking_pb2_grpc
 import json
 
+# The service Booking is also a gRPC server called by service User 
+# Here we define the services provided by the Booking server
+
 class BookingServicer(booking_pb2_grpc.BookingServicer):
 
     def __init__(self):
@@ -16,19 +19,24 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         with open('{}/data/bookings.json'.format("."), 'w') as f:
             json.dump({"bookings" : bookings}, f)
 
+    # get bookings by passing in a user id
     def GetBookingByUserID(self, request, context):
         for booking in self.db:
             if booking['userid'] == request.id:
                 print("User found!")
                 user_schedule_list = []
                 for date_entry in booking['dates']:
+                    # create a DateSchedule object > see booking proto
                     user_schedule = booking_pb2.DateSchedule(date=date_entry['date'], movies=date_entry['movies'])
                     user_schedule_list.append(user_schedule)
                 return booking_pb2.UserBooking(userid=booking['userid'], schedule=user_schedule_list)
+        # set gRPC error code accordingly
         context.set_code(grpc.StatusCode.NOT_FOUND)
         context.set_details('User has no bookings')
+        # return a UserBooking object > see Booking proto
         return booking_pb2.UserBooking(userid="", schedule=[])
 
+    # get all bookings 
     def GetBookings(self, request, context):
         print("All bookings")
         for booking in self.db:
@@ -38,9 +46,12 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
                 user_schedule_list.append(user_schedule)
             yield booking_pb2.UserBooking(userid=booking['userid'], schedule=user_schedule_list)
 
+    # add a booking for a user to the bookings db
     def AddBookingToUser(self, request, context):
+        # prints for clarity and debug
         print("post request")
         print(request.schedule[0].date)
+        # call to the showtime service to check that the date exists and the desired movie is on air at this date
         showtime = run_movie_check_for_date(request.schedule[0].date)
         movie_id = request.schedule[0].movies[0]
         print("check")
@@ -49,19 +60,19 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
             context.set_details('Movie not shown on this day')
             raise ValueError("Movie not available on that day")
         for booking in self.db:
-            print("am i here")
             if booking['userid'] == request.userid:
-                print("ok1")
+                # if the user already has bookings in the db
                 for date in booking['dates']:
-                    print("ok2")
+                    # if they already have bookings on this day
                     if date["date"] == request.schedule[0].date:
-                        print("ok3")
                         if movie_id in date["movies"]:
+                            # if the booking already exists
                             print('booking already registered')
                             context.set_code(grpc.StatusCode.ALREADY_EXISTS)
                             context.set_details('Booking already registered')
                             raise ValueError("Booking already registered")
                         else : 
+                            # add the booking to the db
                             print("adding booking in date")
                             date["movies"].append(movie_id)
                             self.write(self.db)
@@ -70,6 +81,7 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
                                 user_schedule = booking_pb2.DateSchedule(date=date_entry['date'], movies=date_entry['movies'])
                                 user_schedule_list.append(user_schedule)
                             return booking_pb2.UserBooking(userid=booking['userid'], schedule=user_schedule_list)
+                # if the user does not have bookings on this day yet
                 booking["dates"].append({
                 "date" : request.schedule[0].date,
                 "movies" : [movie_id]
@@ -78,6 +90,7 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
                 self.write(self.db)
                 userSchedule = booking_pb2.DateSchedule(date=request.schedule[0].date,movies=[movie_id])
                 return booking_pb2.UserBooking(userid=booking['userid'], schedule=[userSchedule])
+        # if the user has no bookings yet
         self.db.append({
       "userid": request.userid,
       "dates": [
@@ -92,12 +105,13 @@ class BookingServicer(booking_pb2_grpc.BookingServicer):
         userSchedule = booking_pb2.DateSchedule(date=request.schedule[0].date,movies=[movie_id])
         return booking_pb2.UserBooking(userid=request.userid, schedule=[userSchedule])
         return booking_pb2.UserBooking(userid="", schedule=[])
-    
+
+    # get the showtimes by passing in a date
     def GetScheduleByDate(self, request, context):
         print(request.date)
         showtime = run_movie_check_for_date(request.date)
         return showtime
-
+# gRPC server definition and start
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     booking_pb2_grpc.add_BookingServicer_to_server(BookingServicer(), server)
